f8804cd97ef42c3c17300d08da2d226eb39209cb
b54690 <belafonte@hotmail.co.uk>
2019-01-06 20:37:54 +0100

    intial link to songkick API connected

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {RouteReuseStrategy} from '@angular/router';
-
 import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
 import {SplashScreen} from '@ionic-native/splash-screen/ngx';
 import {StatusBar} from '@ionic-native/status-bar/ngx';
-
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import {TabsModule} from "../pages/tabs/tabs.module";
+import {SongkickApiService} from "../services/songkick.api.service";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 @NgModule({
     declarations: [AppComponent],
@@ -17,12 +17,15 @@ import {TabsModule} from "../pages/tabs/tabs.module";
         BrowserModule,
         IonicModule.forRoot(),
         AppRoutingModule,
-        TabsModule
+        TabsModule,
+        HttpClientModule
     ],
     providers: [
         StatusBar,
         SplashScreen,
-        {provide: RouteReuseStrategy, useClass: IonicRouteStrategy}
+        {provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
+        SongkickApiService,
+        HttpClient
     ],
     bootstrap: [AppComponent]
 })
--- src/pages/watchlist/watchlist.component.ts
+++ src/pages/watchlist/watchlist.component.ts
@@ -1,10 +1,29 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SongkickApiService} from "../../services/songkick.api.service";
 
 @Component({
     selector: 'app-watchlist',
     templateUrl: 'watchlist.component.html',
 })
-export class WatchlistComponent {
-    constructor() {
+export class WatchlistComponent implements OnInit {
+    public item: {};
+
+    constructor(
+        private http: SongkickApiService
+    ) {
     }
+
+    public async ngOnInit() {
+        this.getSongKick()
+    }
+
+    private getSongKick(): void {
+        this.http.get().subscribe(
+            (result: {}) => {
+                this.item = result;
+                console.log(this.item)
+            }
+        )
+    }
+
 }
